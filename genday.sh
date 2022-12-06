96eca08835b1eea7791b9a59b1ad40ebe4fd4e5a
Eli Burch <eli.burch@burchbytes.com>
2022-12-05 16:18:57 -0800

    Generate test files also

--- genday.sh
+++ genday.sh
@@ -5,10 +5,14 @@ DAY=$1
 create_package(){
   cp -r "src/main/java/aocdayTEMPLATE" "src/main/java/aocday${DAY}"
   mv src/main/java/aocday"$DAY"/AOCDay{TEMPLATE,"$DAY"}.java
+
+  cp -r "src/test/java/aocdayTEMPLATE" "src/test/java/aocday${DAY}"
+  mv src/test/java/aocday"$DAY"/AOCDay{TEMPLATE,"$DAY"}Tests.java
 }
 
 create_main(){
   sed -i "s/TEMPLATE/$DAY/" src/main/java/aocday"${DAY}"/AOCDay"${DAY}".java
+  sed -i "s/TEMPLATE/$DAY/" src/test/java/aocday"${DAY}"/AOCDay"${DAY}"Tests.java
 }
 
 create_package
---
+++ src/test/java/aocdayTEMPLATE/AOCDayTEMPLATETests.java
@@ -0,0 +1,15 @@
+package aocdayTEMPLATE;
+
+import org.junit.Test;
+
+public class AOCDayTEMPLATETests {
+    @Test
+    public void exampleCaseShouldPassChallenge1(){
+
+    }
+
+    @Test
+    public void exampleCaseShouldPassChallenge2(){
+
+    }
+}
